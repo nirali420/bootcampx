d8d6ab44a3a54033111c3b690fc35fc07848c344
Nirali <nirali420@gmail.com>
2019-11-12 00:17:41 +0000

    added another queries queries_join

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration)
+FROM students
+	JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+	JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
